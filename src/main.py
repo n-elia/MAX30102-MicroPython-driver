6f17ffb2b6bece56f165df5d39ca53003f048138
Nicola Elia <nicolaelia94@gmail.com>
2021-04-27 20:06:03 +0200

    Cleaned

--- src/main.py
+++ src/main.py
@@ -1,21 +1,18 @@
 # main.py
 
-from time import sleep, sleep_ms
-
-# Setup I2C communication
 from MAX30102 import MAX30102
 
 def main():
-  sensor = MAX30102() # Loads default ESP32 I2C configuration
-  # sensor = ParticleSensor(i2cHexAddress = 0x57)
-  # sensor = ParticleSensor(i2cHexAddress = 0x57, i2c = i2cInstance)
-  
-  print("Setting up sensor.", '\n')
-  # Setup the sensor with default configuration
-  sensor.setup_sensor()
-  sensor.setPulseAmplitudeRed(255)
-  sensor.setPulseAmplitudeGreen(0x00)
+	sensor = MAX30102()  # Loads default ESP32 I2C configuration
+	# sensor = ParticleSensor(i2cHexAddress = 0x57)
+	# sensor = ParticleSensor(i2cHexAddress = 0x57, i2c = i2cInstance)
+
+	print("Setting up sensor.", '\n')
+	# Setup the sensor with default configuration
+	sensor.setup_sensor()
+	sensor.setPulseAmplitudeRed(255)
+	sensor.setPulseAmplitudeGreen(0x00)
+
 
 if __name__ == '__main__':
-    sleep_ms(100)
-    main()
+	main()
