805d6477eae7669b8603a7045264c14c27059dbf
Nicola Elia <nicolaelia94@gmail.com>
2021-04-27 20:20:52 +0200

    main reworked for simplicity

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 # main.py
 
-from MAX30102 import MAX30102
+from lib.MAX30102 import MAX30102
 
-def main():
+if __name__ == '__main__':
 	sensor = MAX30102()  # Loads default ESP32 I2C configuration
 	# sensor = ParticleSensor(i2cHexAddress = 0x57)
 	# sensor = ParticleSensor(i2cHexAddress = 0x57, i2c = i2cInstance)
@@ -10,9 +10,3 @@ def main():
 	print("Setting up sensor.", '\n')
 	# Setup the sensor with default configuration
 	sensor.setup_sensor()
-	sensor.setPulseAmplitudeRed(255)
-	sensor.setPulseAmplitudeGreen(0x00)
-
-
-if __name__ == '__main__':
-	main()
