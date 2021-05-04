5d65dbbd3ad748bcdbfc3b34b7b9998e78c16eb1
Nicola <nicolaelia94@gmail.com>
2021-05-04 13:06:59 +0200

    Adapted to the new library methods

--- src/main.py
+++ src/main.py
@@ -1,74 +1,73 @@
 # main.py
 
 from lib.MAX30102 import MAX30102
-from utime import ticks_ms
+from machine import sleep
+from utime import ticks_diff, ticks_ms
 import logging
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     
-    sensor = MAX30102()  # Loads default ESP32 I2C configuration
+    # Sensor instance. If left empty, loads default ESP32 I2C configuration
+    sensor = MAX30102()
+    # Alternatively (for other boards):
     # sensor = ParticleSensor(i2cHexAddress = 0x57)
     # sensor = ParticleSensor(i2cHexAddress = 0x57, i2c = i2cInstance)
     
+    # The default sensor configuration is:
+    # Led mode: 2 (RED + IR)
+    # ADC range: 16384
+    # Sample rate: 400 Hz
+    # Led power: maximum (50.0mA - Presence detection of ~12 inch)
+    # Averaged samples: 8
+    # pulse width: 411
+    
+    # It's possible to setup the sensor at once with the setup_sensor() method.
+    # If no parameters are supplied, the default config is loaded.
+    print("Setting up sensor with default configuration.", '\n')
+    sensor.setup_sensor()
+    
+    # It is also possible to tune the configuration parameters one by one.
     # Set the sample rate to 800: 800 samples/s are collected by the sensor
     sensor.setSampleRate(800)
     # Set the number of samples to be averaged per each reading
     sensor.setFIFOAverage(8)
-
-    print("Setting up sensor.", '\n')
-    # Setup the sensor with default configuration
-    sensor.setup_sensor()
     
+    sleep(1)
+
+    # The readTemperature() method allows to extract the die temperature in °C    
     print("Reading temperature in °C.", '\n')
     print(sensor.readTemperature())
     
-    print("Start data acquisition from RED & IR registers.", '\n')
-    red_array = list()
-    ir_array = list()
+    # Select wether to compute the acquisition frequency or not
+    compute_frequency = False
     
-    t_start = ticks_ms()
-    samples_n=0
+    print("Starting data acquisition from RED & IR registers...", '\n')
+    sleep(1)
     
-    # Select the mode that you want to try:
-    select_mode = 3
-    compute_frequency = True
+    t_start = ticks_ms()    # Starting time of the acquisition
+    samples_n = 0           # Number of samples that has been collected
     
-    if (select_mode == 1):
-        while (True):
-            # Poll continuously the sensor
-            readings = sensor.burst_read()
-            print(readings[0], ",", readings[1])
-    
-    elif (select_mode == 2):
-        while (True):
-            sensor.clearFIFO()
-            for FIFO_pointer in range(32):
-                # Acquire data from the sensor at a given FIFO position
-                sensor_data = sensor.read_FIFO_on_position(FIFO_pointer)
-                if sensor_data is not None:
-                    # Print the acquired data
-                    print(sensor_data[0], ",", sensor_data[1])
-                    # Compute the real acquisition frequency
-                    if (compute_frequency):
-                        samples_n=samples_n+1
-                        if (ticks_ms()-t_start) > 999:
-                            f_HZ = samples_n/1
-                            samples_n = 0
-                            t_start = ticks_ms()
-                            print("acquisition frequency = ",f_HZ)
-    
-    elif (select_mode == 3):
-        while(True):
-            sensor.check()
+    while(True):
+        # The check() method has to be continuously polled, to check if
+        # there are new readings into the sensor's FIFO queue. When new
+        # readings are available, this function will put them into the storage.
+        sensor.check()
+        
+        # Check if the storage contains available samples
+        if(sensor.available()):
+            # Access the storage FIFO and gather the readings (integers)
             red_reading = sensor.popRedFromStorage()
-            if (red_reading != 0):
-                print(red_reading)
-                
-                if (compute_frequency):
-                    samples_n=samples_n+1
-                    if (ticks_ms()-t_start) > 999:
-                        f_HZ = samples_n/1
-                        samples_n = 0
-                        t_start = ticks_ms()
-                        print("acquisition frequency = ",f_HZ)
+            IR_reading = sensor.popIRFromStorage()
+            
+            # Print the acquired data (can be plot with Arduino Serial Plotter)
+            print(red_reading, ",", IR_reading)
+            
+            # We can compute the real frequency at which we receive data
+            if (compute_frequency):
+                samples_n=samples_n+1
+                if ( ticks_diff(ticks_ms(), t_start) > 999 ):
+                    f_HZ = samples_n/1
+                    samples_n = 0
+                    t_start = ticks_ms()
+                    print("acquisition frequency = ",f_HZ)
