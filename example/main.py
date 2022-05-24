a6b27860eb6c499b50c0390c315340aa745c42f4
Nicola <62206011+n-elia@users.noreply.github.com>
2022-05-24 14:57:04 +0200

    Improves error handling, removes default I2C configuration. Updates readme readability, improves example script. Bumps to v0.3.5.

--- example/main.py
+++ example/main.py
@@ -1,15 +1,28 @@
 # main.py
-from machine import sleep
-from utime import ticks_diff, ticks_ms
+from machine import sleep, SoftI2C, Pin
+from utime import ticks_diff, ticks_us
 
 from max30102 import MAX30102
 
 if __name__ == '__main__':
-    # Sensor instance. If left empty, loads default ESP32 I2C configuration
-    sensor = MAX30102()
-    # Alternatively (for other boards):
-    # sensor = MAX30102(i2cHexAddress = 0x57)
-    # sensor = MAX30102(i2cHexAddress = 0x57, i2c = i2cInstance)
+    # I2C software instance
+    i2c = SoftI2C(sda=Pin(22),  # Here, use your I2C SDA pin
+                  scl=Pin(21),  # Here, use your I2C SCL pin
+                  freq=400000)  # Fast: 400kHz, slow: 100kHz
+
+    # Examples of working I2C configurations:
+    # Board             |   SDA pin  |   SCL pin
+    # ------------------------------------------
+    # ESP32 D1 Mini     |   22       |   21
+    # TinyPico ESP32    |   21       |   22
+    # Raspberry Pi Pico |   16       |   17
+
+    # Sensor instance
+    try:
+        sensor = MAX30102(i2c=i2c)  # An I2C instance is required
+    except RuntimeError as err:
+        print("Error occurred while sensor initialization:", err)
+        raise SystemExit(err)
 
     # The default sensor configuration is:
     # Led mode: 2 (RED + IR)
@@ -42,8 +55,8 @@ if __name__ == '__main__':
     print("Starting data acquisition from RED & IR registers...", '\n')
     sleep(1)
 
-    t_start = ticks_ms()  # Starting time of the acquisition
-    samples_n = 0  # Number of samples that has been collected
+    t_start = ticks_us()  # Starting time of the acquisition
+    samples_n = 0  # Number of samples that have been collected
 
     while True:
         # The check() method has to be continuously polled, to check if
@@ -60,11 +73,12 @@ if __name__ == '__main__':
             # Print the acquired data (so that it can be plotted with a Serial Plotter)
             print(red_reading, ",", ir_reading)
 
-            # We can compute the real frequency at which we receive data
+            # Compute the real frequency at which we receive data
             if compute_frequency:
-                samples_n = samples_n + 1
-                if ticks_diff(ticks_ms(), t_start) > 999:
-                    f_HZ = samples_n / 1
+                if ticks_diff(ticks_us(), t_start) >= 999999:
+                    f_HZ = samples_n
                     samples_n = 0
-                    t_start = ticks_ms()
                     print("acquisition frequency = ", f_HZ)
+                    t_start = ticks_us()
+                else:
+                    samples_n = samples_n + 1
